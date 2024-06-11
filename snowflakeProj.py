02f2b2d80368a9ea443688f636e7cc6e66a8ed93
AntoSynergy <168104147+AntoSynergy@users.noreply.github.com>
2024-06-11 13:47:10 +0200

    Update snowflakeProj.py

--- snowflakeProj.py
+++ snowflakeProj.py
@@ -12,8 +12,17 @@ elif Page=="Dépôt":
   file_upload=st.file_uploader("Selectionnez le fichier CSV à upload",type="CSV")
   delimiter=st.selectbox("Choisissez le delimiter du fichier",[",",";"," ","-","_"])
   if file_upload is not None:   
-    
-    bytes_data=file_upload.read()
-    bytes_data.show()
-    validate=st.button("Valider le téléchargement")
-  
+            # Charger le fichier CSV dans un DataFrame Pandas
+    try:
+         if uploaded_file.name.endswith('.csv'):
+              df = pd.read_csv(uploaded_file, sep=delimiter)
+         elif uploaded_file.name.endswith('.xlsx'):
+              df = pd.read_excel(uploaded_file)
+         else:
+              st.error("Format de fichier non pris en charge.")
+              st.stop()
+                
+                # Afficher les premières lignes du DataFrame
+         st.write("Aperçu des données :")
+         st.write(df.head())
+      
