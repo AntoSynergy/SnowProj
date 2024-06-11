4cf410674daf72b78abd6f9910d273d12d5c7db3
AntoSynergy <168104147+AntoSynergy@users.noreply.github.com>
2024-06-11 12:12:25 +0200

    Update snowflakeProj.py

--- snowflakeProj.py
+++ snowflakeProj.py
@@ -6,6 +6,6 @@ import streamlit as st
 st.title("Bienvenue")
 Page=st.sidebar.selectbox("Veuillez choisir votre page:",["Accueil","Dépôt"])
 if Page=="Accueil": 
-  st.title("Bienvenue sur le dépôt officiel dédié à téléversé des fichiers sur Snowflake")
+  st.title("Bienvenue sur le dépôt officiel dédié à téléverser des fichiers sur Snowflake")
 elif Page=="Dépôt": 
   st.title("Ici vous pouvez faire votre téléversement")
