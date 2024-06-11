64a264859a766c2f94b2f8b05bac50dc6f5cf65c
AntoSynergy <168104147+AntoSynergy@users.noreply.github.com>
2024-06-11 13:49:19 +0200

    Update snowflakeProj.py

--- snowflakeProj.py
+++ snowflakeProj.py
@@ -11,18 +11,29 @@ elif Page=="Dépôt":
   st.subheader("Ici vous pouvez faire votre téléversement")
   file_upload=st.file_uploader("Selectionnez le fichier CSV à upload",type="CSV")
   delimiter=st.selectbox("Choisissez le delimiter du fichier",[",",";"," ","-","_"])
-  if file_upload is not None:   
-            # Charger le fichier CSV dans un DataFrame Pandas
-    try:
-      if uploaded_file.name.endswith('.csv'):
-        df = pd.read_csv(uploaded_file, sep=delimiter)
-      elif uploaded_file.name.endswith('.xlsx'):
-        df = pd.read_excel(uploaded_file)
-      else:
-        st.error("Format de fichier non pris en charge.")
-        st.stop()
-                
-                # Afficher les premières lignes du DataFrame
-    st.write("Aperçu des données :")
-    st.write(df.head())
+ if file_upload is not None:
+        bytes_data = file_upload.read()
+        s = str(bytes_data, 'utf-8')
+        data = StringIO(s)
+        
+        # Afficher l'aperçu des données en fonction du délimiteur choisi
+        try:
+            df = pd.read_csv(data, delimiter=delimiter)
+            st.write("Aperçu du fichier téléversé :")
+            st.dataframe(df)
+        except Exception as e:
+            st.error(f"Erreur lors de la lecture du fichier : {e}")
+        
+        table_name = st.text_input("Nom de la table dans Snowflake :")
+        
+        if st.button('Confirmer le téléversement'):
+            if table_name:
+                try:
+                    conn = get_snowflake_connection()
+                    upload_to_snowflake(conn, df, table_name)
+                    st.success(f"Les données ont été téléversées dans la table {table_name} de Snowflake avec succès.")
+                except Exception as e:
+                    st.error(f"Erreur lors du téléversement des données : {e}")
+            else:
+                st.error("Veuillez saisir un nom de table.")
       
