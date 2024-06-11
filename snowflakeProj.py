66426ea850cc4656e16248b83ee1f32934307867
AntoSynergy <168104147+AntoSynergy@users.noreply.github.com>
2024-06-11 16:14:20 +0200

    Update snowflakeProj.py

--- snowflakeProj.py
+++ snowflakeProj.py
@@ -10,7 +10,7 @@ import seaborn as sns
 # connection à snowflake 
 
 conn=st.connection("Snowflake")
-session=conn.session()
+#session=conn.session()
 
 # Fonction pour téléverser un DataFrame dans Snowflake
 def upload_to_snowflake(conn, df, table_name):
