4d2dcc3cd5f4eea178d8fe255ea0726bee961705
AntoSynergy <168104147+AntoSynergy@users.noreply.github.com>
2024-06-11 13:56:37 +0200

    Update snowflakeProj.py

--- snowflakeProj.py
+++ snowflakeProj.py
@@ -14,6 +14,9 @@ elif page == "Dépôt":
     
     file_upload = st.file_uploader("Sélectionnez le fichier CSV à upload", type="csv")
     delimiter = st.selectbox("Choisissez le délimiteur du fichier", [",", ";","_"])
+    quotechar = st.selectbox("Choisissez le caractère de citation", ['"', "'", ""])
+    skip_rows = st.number_input("Nombre de lignes à ignorer au début", min_value=0, step=1, value=0)
+    skip_blank_lines = st.checkbox("Ignorer les lignes blanches", value=True)
     
     if file_upload is not None:
         bytes_data = file_upload.read()
@@ -22,7 +25,13 @@ elif page == "Dépôt":
         
         # Afficher l'aperçu des données en fonction du délimiteur choisi
         try:
-            df = pd.read_csv(data, delimiter=delimiter)
+            df = pd.read_csv(
+                data, 
+                delimiter=delimiter, 
+                quotechar=quotechar if quotechar else None,
+                skiprows=skip_rows,
+                skip_blank_lines=skip_blank_lines
+            )
             st.write("Aperçu du fichier téléversé :")
             st.dataframe(df)
         except Exception as e:
